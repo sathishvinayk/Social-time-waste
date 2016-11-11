73ae4339f598ac10edd166804c48bc456071549c
sathishvinayk <sathishvinayk@gmail.com>
2016-11-11 20:58:33 +0530

    Add files via upload
    Added layouts

---
+++ server.js
@@ -0,0 +1,16 @@
+var express=require('express'),
+    mongoose=require('mongoose'),
+    bodyParser=require('body-parser'),
+    app=express();
+
+mongoose.connect('mongodb://localhost:27017/time-waste');
+
+app.use('/app',express.static(__dirname+'/app'));
+app.use('/node_modules',express.static(__dirname+ '/node_modules'));
+
+app.get('/',function(req,res){
+    res.sendfile('index.html');
+});
+app.listen('3000',function(){
+    console.log('Running local 3000');
+});
